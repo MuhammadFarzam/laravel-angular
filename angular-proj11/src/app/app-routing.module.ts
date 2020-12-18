387613f3563fc4f9506c63895408065fc8049084
Muhammad Farzam <farzam.ajmal@shispare.com>
2020-12-18 12:58:49 -0800

    Add Routing in Angular JWT

---
+++ angular-proj11/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
+//import { CommonModule } from '@angular/common';
+//import { RouteModules, Routes } from '@angular/router';
+
+
+const appRoutes:Routes = [
+  {
+     path: 'login',
+     component: LoginComponent
+  },
+  {
+     path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'request-password-reset',
+    component: RequestResetComponent
+  },
+  {
+    path: 'response-password-reset',
+    component: ResponseResetComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
